7383ce147c343d582808920eb5112d1ddbb3e277
yash chandra <91049111+chandrayash525@users.noreply.github.com>
2021-10-10 20:05:03 +0530

    Add files via upload

---
+++ power of appprox.py
@@ -0,0 +1,22 @@
+
+def approx():
+    import random
+    print("length of one symbol ","                      ","-")
+    ch1=random.randrange(100,400)
+    print("-"*ch1)
+    ans=[]
+    ch=int(input("enter number of     -  there is -10 to +10 error is allow or you fail"))
+    if(ch>ch1):
+            e=ch-ch1
+    else:
+            e=ch1-ch
+    if(ch1 > ch +10 or ch1<ch-10):
+        print("Fail your points is ",1/e,"actual number of   - is                  ",ch1)
+    else:
+        if(ch==ch1):
+            print("you are 100%  accurate")
+        else:
+            print("you pass  but  error of ",e,"your points",1/e,"actual number of   - is                  ",ch1)
+    print(ch1)
+approx()
+            
